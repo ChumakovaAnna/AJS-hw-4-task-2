b24023956aebd3f3cf2595013f054d92da328d2d
Anna Chumakova <chumakova3008@gmail.com>
2020-05-11 18:40:53 +0500

    Обновлен config и переписана findBy стрелочными функциями

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
-import findBy from './basic';
+import findBy from "./findBy";
 
-console.log('worked');
+console.log("worked");
 
 const arrForSearch = [
-  { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
-  { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
-  { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+  { name: "маг", type: "character", description: "Персонаж, обладающий магическими способностями" },
+  { name: "заклинание", type: "attack", description: "Атака магическим заклинанием" },
+  { name: "урон", type: "help", description: "Страница описания элемента интерфейса" },
 ];
 
-console.log(arrForSearch.filter(findBy('name', 'урон')));
+const finder = findBy("name", "урон");
+console.log(arrForSearch.filter(finder));
--- src/js/basic.js
+++
@@ -1,3 +0,0 @@
-export default function findBy(key, value) {
-  return (item) => item[key] === value;
-}
